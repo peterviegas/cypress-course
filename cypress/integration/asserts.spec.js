0cd19ec93f1392068ab22d9a5101dcfd2283c7a8
Peter Viegas <peterviegas@outlook.com>
2023-06-04 07:51:30 +0100

    Checking if it's true

--- cypress/integration/asserts.spec.js
+++ cypress/integration/asserts.spec.js
@@ -11,3 +11,16 @@ it('Equality', () => {
     expect(a).to.be.equal(1);
     expect('a').not.to.be.equal('b')
 })
+
+it('Truthy', () => {
+    const a = true;
+    const b = null;
+    let c;
+
+    expect(a).to.be.true;
+    expect(true).to.be.true;
+    expect(false).not.to.be.true;
+    expect(b).to.be.null;
+    expect(a).to.be.not.null;
+    expect(c).to.be.undefined;
+})
