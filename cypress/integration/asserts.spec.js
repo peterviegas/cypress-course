fb6949692eac8f3056482c242e521574fbac1fa0
Peter Viegas <peterviegas@outlook.com>
2023-06-04 08:35:32 +0100

    Object Equality

--- cypress/integration/asserts.spec.js
+++ cypress/integration/asserts.spec.js
@@ -24,3 +24,25 @@ it('Truthy', () => {
     expect(a).to.be.not.null;
     expect(c).to.be.undefined;
 })
+
+it('Object Equality', () => {
+    const obj = {
+        a: 1,
+        b: 2
+    }
+
+    expect(obj).equal(obj);
+    expect(obj).equals(obj);
+    expect(obj).eq(obj)
+    expect(obj).to.be.equal(obj)
+    expect(obj).to.be.deep.equal({ a: 1, b: 2 }) //verifica a propriedade do objeto
+    expect(obj).eql({ a: 1, b: 2 }) //verifica a propriedade do objeto
+
+    //Quando o objeto é grande e voce que verificar apenas parte dele.
+    expect(obj).include({ a: 1 })
+    expect(obj).to.have.property('b', 2) //Verifica a existência da propriedade e que seu valor é
+
+    expect(obj).to.not.be.empty
+    expect({}).to.be.empty
+
+})
