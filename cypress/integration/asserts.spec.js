a1438051294545fa25410db3918bf85ff7bb5675
Peter Viegas <peterviegas@outlook.com>
2023-06-04 08:40:24 +0100

    Checking Arrays

--- cypress/integration/asserts.spec.js
+++ cypress/integration/asserts.spec.js
@@ -46,3 +46,12 @@ it('Object Equality', () => {
     expect({}).to.be.empty
 
 })
+
+it('Arrays', () => {
+    const arr = [1, 2, 3]
+
+    expect(arr).to.have.members([1, 2, 3])
+    expect(arr).to.include.members([1, 3])
+    expect(arr).to.not.be.empty
+    expect([]).to.be.empty
+})
